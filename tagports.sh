8b1c1351bcd470b6cf7a765f3c70869e690d097e
pkgdemon <pkgdemonteam@gmail.com>
2019-11-28 07:52:03 -0500

    Use version string that will allow master to be upgraded

--- tagports.sh
+++ tagports.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 # Helper script which will tag github repos for ports / distfiles
 
-if [ -z "$1" ] ; then
-  echo "Please provide tag"
-  echo "For example 11-28-2019"
-  exit 1
-fi
-
 # Only run as regular user with ssh keys
 if [ "$(id -u)" = "0" ]; then
   echo "This script must be run by release engineer" 1>&2
@@ -23,6 +17,13 @@ repodir="/tmp/furybsd-tag"
 rm -rf ${repodir}
 mkdir -p ${repodir}
 
+if [ ! -f "/usr/local/furybsd/version" ] ; then
+  echo "FuryBSD must be built with tag first"
+  exit 1
+fi
+
+version=`cat /usr/local/furybsd/version`
+
 git clone git@github.com:furybsd/furybsd-update.git ${repodir}/furybsd-update
 git clone git@github.com:furybsd/furybsd-xorg-tool.git ${repodir}/furybsd-xorg-tool
 git clone git@github.com:furybsd/furybsd-xfce-settings.git ${repodir}/furybsd-xfce-settings
@@ -30,18 +31,18 @@ git clone git@github.com:furybsd/furybsd-wallpapers.git ${repodir}/furybsd-wallp
 git clone git@github.com:furybsd/furybsd-common-settings.git ${repodir}/furybsd-common-settings
 
 cd ${repodir}/furybsd-update
-git tag -a $1 -m "{$1}"
+git tag -a $version -m "{$version}"
 git push origin --tags
 cd ${repodir}/furybsd-xorg-tool
-git tag -a $1 -m "{$1}"
+git tag -a $version -m "{$version}"
 git push origin --tags
 cd ${repodir}/furybsd-xfce-settings
-git tag -a $1 -m "{$1}"
+git tag -a $version -m "{$version}"
 git push origin --tags
 cd ${repodir}/furybsd-wallpapers
-git tag -a $1 -m "{$1}"
+git tag -a $version -m "{$version}"
 git push origin --tags
 cd ${repodir}/furybsd-common-settings
-git tag -a $1 -m "{$1}"
+git tag -a $version -m "{$version}"
 git push origin --tags
 rm -rf ${repodir}
